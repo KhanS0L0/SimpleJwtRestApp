eb755066dabc9dd69d9f6d47cb57942752eee846
KhanS0L0 <hans12.study@gmail.com>
2022-01-20 21:46:55 +0300

    Simple authen / author with JWT + Spring (Boot / Security)

---
+++ src/main/java/com/example/dto/AdminUserDTO.java
@@ -0,0 +1,39 @@
+package com.example.dto;
+
+import com.example.model.Status;
+import com.example.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AdminUserDTO {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String status;
+
+    public User toUser() {
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setStatus(Status.valueOf(status));
+        return user;
+    }
+
+    public static AdminUserDTO fromUser(User user) {
+        AdminUserDTO adminUserDto = new AdminUserDTO();
+        adminUserDto.setId(user.getId());
+        adminUserDto.setUsername(user.getUsername());
+        adminUserDto.setFirstName(user.getFirstName());
+        adminUserDto.setLastName(user.getLastName());
+        adminUserDto.setEmail(user.getEmail());
+        adminUserDto.setStatus(user.getStatus().name());
+        return adminUserDto;
+    }
+}
---
+++ src/main/java/com/example/model/Status.java
@@ -0,0 +1,4 @@
+package com.example.model;
+public enum Status {
+    ACTIVE, NOT_ACTIVE, DELETED
+}
